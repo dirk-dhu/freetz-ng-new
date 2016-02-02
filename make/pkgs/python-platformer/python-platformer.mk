4bcc18841a0fef9bac96f4b4b4e205ade50682d4
Dirk <dirk-dhu@users.noreply.github.com>
2016-02-02 22:59:50 +0100

    python: add python modules pyusb, pyyaml, lxml, configobj, six, pygooglechart, certbot acme, zope, mock, ndg-httsclient, pyasn, pycparser, idna, augeas and much more

---
+++ make/pkgs/python-platformer/python-platformer.mk
@@ -0,0 +1,32 @@
+$(call PKG_INIT_BIN,  48855cd)
+$(PKG)_SOURCE:=platformer-$($(PKG)_VERSION).tar.gz
+$(PKG)_SITE:=git@https://github.com/fijal/platformer.git
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/platformer-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/platformer
+
+$(PKG)_DEPENDS_ON += python
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_PLATFORMER)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_PLATFORMER_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PLATFORMER_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/platformer \
+		$(PYTHON_PLATFORMER_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/platformer-*.egg-info
+
+$(PKG_FINISH)
