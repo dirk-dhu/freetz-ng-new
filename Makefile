Coolzero82/freetz

NATIVE COMPILER: add target compiler and target tools as m4, automake, autoconf, bison and flex as a package, can be used as external and fix build problems also for Microsoft kernel

Makefile
@@ -237,8 +237,13 @@ TOOLCHAIN_DISTCLEAN:=$(patsubst %,%-distclean,$(TOOLCHAIN))
 TOOLCHAIN_SOURCE:=$(patsubst %,%-source,$(TOOLCHAIN))
 
 ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN)),y)
+ifneq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
 include $(TOOLCHAIN_DIR)/make/kernel-toolchain.mk
+endif
 include $(TOOLCHAIN_DIR)/make/target-toolchain.mk
+ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
+include $(TOOLCHAIN_DIR)/make/download-toolchain.mk
+endif
 else
 include $(TOOLCHAIN_DIR)/make/download-toolchain.mk
 endif

dirk-dhu/freetz-ng-new

Makefile
@@ -371,8 +371,13 @@ TOOLCHAIN_DISTCLEAN:=$(patsubst %,%-distclean,$(TOOLCHAIN))
 TOOLCHAIN_SOURCE:=$(patsubst %,%-source,$(TOOLCHAIN))
 
 ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN)),y)
+ifneq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
 include $(MAKE_DIR)/toolchain/kernel-toolchain.mk
+endif
 include $(MAKE_DIR)/toolchain/target-toolchain.mk
+ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
+include $(MAKE_DIR)/toolchain/download-toolchain.mk
+endif
 else
 include $(MAKE_DIR)/toolchain/download-toolchain.mk
 endif