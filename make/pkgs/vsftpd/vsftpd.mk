e526643cd90404869e1c19cfde49e88facb17523
fda77 <fda77@users.noreply.github.com>
2023-03-18 16:18:35 +0100

    vsftpd: add links

--- make/pkgs/vsftpd/vsftpd.mk
+++ make/pkgs/vsftpd/vsftpd.mk
@@ -2,6 +2,9 @@ $(call PKG_INIT_BIN, 3.0.5)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_HASH:=26b602ae454b0ba6d99ef44a09b6b9e0dfa7f67228106736df1f278c70bc91d3
 $(PKG)_SITE:=https://security.appspot.com/downloads
+### WEBSITE:=http://vsftpd.beasts.org/
+### MANPAGE:=https://security.appspot.com/vsftpd/vsftpd_conf.html
+### CHANGES:=https://security.appspot.com/vsftpd/Changelog.txt
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/vsftpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/vsftpd
@@ -21,6 +24,7 @@ ifeq ($(strip $(FREETZ_PACKAGE_VSFTPD_STATIC)),y)
 $(PKG)_LDFLAGS += -static
 endif
 
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
@@ -38,6 +42,7 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(VSFTPD_DIR) clean
