5533fbb33347781790a04f6ff26d33f895a88f4c
fda77 <fda77@users.noreply.github.com>
2023-05-25 07:57:25 +0200

    cmake: install to build/

--- make/host-tools/cmake-host/cmake-host.mk
+++ make/host-tools/cmake-host/cmake-host.mk
@@ -7,27 +7,22 @@ $(PKG)_SITE:=https://github.com/Kitware/CMake/releases/download/v$($(PKG)_VERSIO
 ### MANPAGE:=https://cmake.org/cmake/help/latest/
 ### CHANGES:=https://github.com/Kitware/CMake/releases
 ### CVSREPO:=https://gitlab.kitware.com/cmake/cmake
-# needs:  export CMAKE_ROOT=~/freetz-ng/tools/build/usr/share/cmake-3.25
 
-$(PKG)_SHARE_NAME:=cmake-$($(PKG)_MAJOR_VERSION)
 $(PKG)_INSTALL:=$($(PKG)_DIR)/INSTALL
 
 $(PKG)_BINARY:=$($(PKG)_INSTALL)/bin/cmake
-$(PKG)_TARGET_BINARY:=$(TOOLS_DIR)/cmake
+$(PKG)_TARGET_BINARY:=$(TOOLS_DIR)/build/bin/cmake
 
-$(PKG)_SHARE:=$($(PKG)_INSTALL)/share
-$(PKG)_TARGET_SHARE:=$(TOOLS_DIR)/build/usr/share
-
-$(PKG)_SHARE_INCLUDE:=$($(PKG)_INSTALL)/share/$($(PKG)_SHARE_NAME)/include/cmCPluginAPI.h
-$(PKG)_TARGET_SHARE_INCLUDE:=$($(PKG)_TARGET_SHARE)/$($(PKG)_SHARE_NAME)/include/cmCPluginAPI.h
+$(PKG)_SHARE:=$($(PKG)_INSTALL)/share/cmake-$($(PKG)_MAJOR_VERSION)
+$(PKG)_TARGET_SHARE:=$(TOOLS_DIR)/build/share/cmake-$($(PKG)_MAJOR_VERSION)
 
 $(PKG)_DEPENDS_ON+=ninja-host
 
-$(PKG)_CONFIGURE_OPTIONS +=  --generator=Ninja
-$(PKG)_CONFIGURE_OPTIONS +=  --enable-ccache
-$(PKG)_CONFIGURE_OPTIONS +=  --no-qt-gui
-$(PKG)_CONFIGURE_OPTIONS +=  --no-system-libs
-$(PKG)_CONFIGURE_OPTIONS +=  --prefix=$($(PKG)_INSTALL)
+$(PKG)_CONFIGURE_OPTIONS += --generator=Ninja
+$(PKG)_CONFIGURE_OPTIONS += --enable-ccache
+$(PKG)_CONFIGURE_OPTIONS += --no-qt-gui
+$(PKG)_CONFIGURE_OPTIONS += --no-system-libs
+$(PKG)_CONFIGURE_OPTIONS += --prefix=$($(PKG)_INSTALL)
 
 
 $(TOOLS_SOURCE_DOWNLOAD)
@@ -42,26 +37,26 @@ $($(PKG)_DIR)/.installed: $($(PKG)_DIR)/.compiled
 	$(TOOLS_SUBNINJA) -C $(CMAKE_HOST_DIR) install
 	@touch $@
 
-$($(PKG)_BINARY) $($(PKG)_SHARE_INCLUDE): $($(PKG)_DIR)/.installed
+$($(PKG)_BINARY) $($(PKG)_SHARE): $($(PKG)_DIR)/.installed
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_FILE)
 
-$($(PKG)_TARGET_SHARE_INCLUDE): $($(PKG)_SHARE_INCLUDE)
-	mkdir -p $(CMAKE_HOST_TARGET_SHARE)/;
-	cp -r $(CMAKE_HOST_SHARE)/$(CMAKE_HOST_SHARE_NAME) $(CMAKE_HOST_TARGET_SHARE)/;
+$($(PKG)_TARGET_SHARE): $($(PKG)_SHARE)
+	mkdir -p $(dir $(CMAKE_HOST_TARGET_SHARE))
+	cp -r $(CMAKE_HOST_SHARE) $(dir $(CMAKE_HOST_TARGET_SHARE))
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_SHARE_INCLUDE)
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY) $($(PKG)_TARGET_SHARE)
 
 
 $(pkg)-clean:
 	-$(NINJA) -C $(CMAKE_HOST_DIR) uninstall
-	 $(RM) $(NCFTP_DIR)/.{configured,compiled,installed}
+	-$(RM) $(CMAKE_HOST_DIR)/.{configured,compiled,installed}
 
 $(pkg)-dirclean:
 	$(RM) -r $(CMAKE_HOST_DIR)
 
 $(pkg)-distclean: $(pkg)-dirclean
-	$(RM) -r $(CMAKE_HOST_TARGET_BINARY) $(CMAKE_HOST_TARGET_SHARE)/$(CMAKE_HOST_SHARE_NAME)
+	$(RM) -r $(CMAKE_HOST_TARGET_BINARY) $(CMAKE_HOST_TARGET_SHARE)
 
 $(TOOLS_FINISH)
