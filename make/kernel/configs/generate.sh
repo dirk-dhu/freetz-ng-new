f14babe790dd8a8f63098a472428c9d04926c86d
fda77 <fda77@users.noreply.github.com>
2023-07-02 20:35:46 +0200

    kernel: add generate_diffs

---
+++ make/kernel/configs/generate.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+MYPWD="$(dirname $(realpath $0))"
+cd "$MYPWD"
+
+for i in avm/config-*; do
+	[ "$i" != "${i%--not-available}" ] && continue
+	i=$(basename $i)
+	sed -r -e 's,=n$,=m,' "avm/$i" | diff -du --label "avm/$i" --label "freetz/$i" - freetz/$i > diffs/$i.diff
+done
