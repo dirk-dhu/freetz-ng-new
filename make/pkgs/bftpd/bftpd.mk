6fe919dd8cb9d6a07801725c169d8f20a79fcab7
fda77 <fda77@users.noreply.github.com>
2023-07-06 18:53:08 +0200

    bftpd: add links

--- make/pkgs/bftpd/bftpd.mk
+++ make/pkgs/bftpd/bftpd.mk
@@ -2,6 +2,10 @@ $(call PKG_INIT_BIN, 3.3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_HASH:=c5c7c074623a6cf0a4682e6f769b1aebfc8ef69150204fbe071169d83ceb3435
 $(PKG)_SITE:=@SF/bftpd
+### WEBSITE:=https://bftpd.sourceforge.net/
+### MANPAGE:=https://bftpd.sourceforge.net/documents.html
+### CHANGES:=https://bftpd.sourceforge.net/downloads/CHANGELOG
+### CVSREPO:=https://sourceforge.net/projects/bftpd/
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/bftpd
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/bftpd
@@ -13,6 +17,7 @@ endif
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_BFTPD_WITH_ZLIB
 
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_BFTPD_WITH_ZLIB),--enable-libz)
+
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -27,6 +32,7 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
 
 $(pkg)-clean:
 	-$(SUBMAKE) -C $(BFTPD_DIR) clean
