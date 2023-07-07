32827575bfe506290bc809d1702c423e732ed8e4
fda77 <fda77@users.noreply.github.com>
2023-07-07 13:16:54 +0200

    tar: add and use some variables

--- make/host-tools/tar-host/tar-host.mk
+++ make/host-tools/tar-host/tar-host.mk
@@ -3,9 +3,14 @@ $(PKG)_SOURCE:=tar-$($(PKG)_VERSION).tar.xz
 $(PKG)_HASH:=63bebd26879c5e1eea4352f0d03c991f966aeb3ddeb3c7445c902568d5411d28
 $(PKG)_SITE:=@GNU/tar
 
+$(PKG)_PREFIX:=$($(PKG)_DIR)/._INSTALL
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/tar
+$(PKG)_TARGET_BINARY:=$(TOOLS_DIR)/tar-gnu
+
 $(PKG)_DEPENDS_ON:=kconfig-host
 
-$(PKG)_CONFIGURE_OPTIONS += --prefix=/usr
+$(PKG)_CONFIGURE_OPTIONS += --prefix=$($(PKG)_PREFIX)
 $(PKG)_CONFIGURE_OPTIONS += --without-selinux
 $(PKG)_CONFIGURE_OPTIONS += --disable-acl
 
@@ -18,14 +23,14 @@ $(TOOLS_SOURCE_DOWNLOAD)
 $(TOOLS_UNPACKED)
 $(TOOLS_CONFIGURED_CONFIGURE)
 
-$($(PKG)_DIR)/src/tar: $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(TOOLS_SUBMAKE) -C $(TAR_HOST_DIR) all
 	touch -c $@
 
-$(TOOLS_DIR)/tar-gnu: $($(PKG)_DIR)/src/tar
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_FILE)
 
-$(pkg)-precompiled: $(TOOLS_DIR)/tar-gnu
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 
 $(pkg)-clean:
@@ -35,6 +40,6 @@ $(pkg)-dirclean:
 	$(RM) -r $(TAR_HOST_DIR)
 
 $(pkg)-distclean: $(pkg)-dirclean
-	$(RM) $(TOOLS_DIR)/tar-gnu
+	$(RM) $(TAR_TARGET_BINARY)
 
 $(TOOLS_FINISH)
