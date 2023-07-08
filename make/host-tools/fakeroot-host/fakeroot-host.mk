4d9fcc8f0dfa383fa513f148b15b4437251184d3
fda77 <fda77@users.noreply.github.com>
2023-07-08 11:01:53 +0200

    fakeroot: simplification

--- make/host-tools/fakeroot-host/fakeroot-host.mk
+++ make/host-tools/fakeroot-host/fakeroot-host.mk
@@ -72,16 +72,13 @@ $($(PKG)_TARGET_BIARCH_LIB): $($(PKG)_BIARCH_DIR)/.configured
 	$(TOOLS_SUBMAKE) -C $(FAKEROOT_HOST_BIARCH_DIR) libdir="$(FAKEROOT_HOST_BIARCH_LD_PRELOAD_PATH)" install-libLTLIBRARIES
 	touch $@
 
-$(pkg)-fixhardcoded: $($(PKG)_FIXHARDCODED)
-$($(PKG)_FIXHARDCODED):
-	@ \
-	[ -d "$(FAKEROOT_HOST_DIR)" ] || \
-	sed "s!/home/freetz/freetz-ng/tools/build!$(realpath tools/build/)!g" -i \
-	  $(FAKEROOT_HOST_MAINARCH_LD_PRELOAD_PATH)/libfakeroot.la \
-	  $(FAKEROOT_HOST_BIARCH_LD_PRELOAD_PATH)/libfakeroot.la
+$(pkg)-fixhardcoded:
+	@$(SED) -i "s!/home/freetz/freetz-ng/tools/build!$(realpath tools/build/)!g" \
+		$(FAKEROOT_HOST_MAINARCH_LD_PRELOAD_PATH)/libfakeroot.la \
+		$(FAKEROOT_HOST_BIARCH_LD_PRELOAD_PATH)/libfakeroot.la
 	touch $@
 
-$(pkg)-precompiled: $($(PKG)_TARGET_SCRIPT) $(if $(BIARCH_BUILD_SYSTEM),$($(PKG)_TARGET_BIARCH_LIB)) $($(PKG)_FIXHARDCODED)
+$(pkg)-precompiled: $($(PKG)_TARGET_SCRIPT) $(if $(BIARCH_BUILD_SYSTEM),$($(PKG)_TARGET_BIARCH_LIB))
 
 
 $(pkg)-clean:
@@ -92,6 +89,9 @@ $(pkg)-dirclean:
 	$(RM) -r $(FAKEROOT_HOST_DIR)
 
 $(pkg)-distclean: $(pkg)-dirclean
-	$(RM) -r $(FAKEROOT_HOST_TARGET_SCRIPT) $(FAKEROOT_HOST_DESTDIR)/bin/faked $(FAKEROOT_HOST_DESTDIR)/lib*/libfakeroot*
+	$(RM) -r \
+		$(FAKEROOT_HOST_TARGET_SCRIPT) \
+		$(FAKEROOT_HOST_DESTDIR)/bin/faked \
+		$(FAKEROOT_HOST_DESTDIR)/lib*/libfakeroot*
 
 $(TOOLS_FINISH)
