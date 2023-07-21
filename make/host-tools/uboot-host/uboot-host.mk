cba3d94040323a8f0da3159736a0e7a8ff0d639e
fda77 <fda77@users.noreply.github.com>
2023-07-21 12:37:17 +0200

    uboot: use openssl-host

--- make/host-tools/uboot-host/uboot-host.mk
+++ make/host-tools/uboot-host/uboot-host.mk
@@ -7,23 +7,31 @@ $(PKG)_SITE:=git@https://github.com/u-boot/u-boot.git
 
 $(PKG)_DESTDIR:=$(FREETZ_BASE_DIR)/$(TOOLS_DIR)/fit
 
+$(PKG)_DEPENDS_ON+=$(if $(FREETZ_TOOLS_UBOOT_STATIC),openssl-host)
+
 $(PKG)_BINARIES            := dumpimage fdtgrep mkimage
 $(PKG)_BINARIES_BUILD_DIR  := $($(PKG)_BINARIES:%=$($(PKG)_DIR)/tools/%)
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DESTDIR)/%)
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_TOOLS_UBOOT_STATIC
-$(PKG)_CONDITIONAL_PATCHES+=$(if $(FREETZ_TOOLS_UBOOT_STATIC),static_openssl)
+ifeq ($(FREETZ_TOOLS_UBOOT_STATIC),y)
+$(PKG)_CONDITIONAL_PATCHES+=static_openssl
+$(PKG)_MAKE_VARS += HOSTLDFLAGS="-L$(OPENSSL_HOST_DIR)"
+endif
 
 
 $(TOOLS_SOURCE_DOWNLOAD)
 $(TOOLS_UNPACKED)
 
 $($(PKG)_DIR)/.configured: $($(PKG)_DIR)/.unpacked
-	$(TOOLS_SUBMAKE) -C $(UBOOT_HOST_DIR) tools-only_defconfig
+	$(TOOLS_SUBMAKE) -C $(UBOOT_HOST_DIR) \
+		tools-only_defconfig
 	touch $@
 
 $($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
-	$(TOOLS_SUBMAKE) -C $(UBOOT_HOST_DIR) tools-only
+	$(TOOLS_SUBMAKE) -C $(UBOOT_HOST_DIR) \
+		$(UBOOT_HOST_MAKE_VARS) \
+		tools-only
 
 $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DESTDIR)/%: $($(PKG)_DIR)/tools/%
 	$(INSTALL_FILE)
