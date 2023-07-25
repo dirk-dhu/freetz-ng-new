4b6734748fd552fdaec7a4af9a5bdbc224981e9b
fda77 <fda77@users.noreply.github.com>
2023-07-25 23:39:27 +0200

    bump sudo

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -98,7 +98,7 @@ Latest changes
    * SQLite 3.42.0
    * strace 4.9/5.0/6.4
    * Stunnel 5.70
-   * Sudo 1.9.14p2
+   * Sudo 1.9.14p3
    * Transmission 3.00/4.0.3
    * UnRAR 6.1.7/6.2.8
    * vnstat 1.18
--- make/pkgs/sudo/sudo.mk
+++ make/pkgs/sudo/sudo.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 1.9.14p2)
+$(call PKG_INIT_BIN, 1.9.14p3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_HASH:=15f6308db0ed4265ec929f2c61e2e13492e551750c203ee001d1fcd8104c9514
+$(PKG)_HASH:=a08318b1c4bc8582c004d4cd9ae2903abc549e7e46ba815e41fe81d1c0782b62
 $(PKG)_SITE:=https://www.sudo.ws/dist
 ### WEBSITE:=https://www.sudo.ws/
 ### MANPAGE:=https://www.sudo.ws/docs/man/sudoers.man/
