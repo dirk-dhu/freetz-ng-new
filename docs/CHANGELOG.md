dac50c8a58ad4cd7f6f7f1b55888c94b4dada86b
harryboo <jens.gutsche@gmx.de>
2023-07-29 21:17:26 +0200

    bump openssh (#822)

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -92,6 +92,7 @@ Latest changes
    * iPerf3 3.3/3.14
    * minidlna 1.3.3
    * NZBGet 21.1/21.4-rc1
+   * OpenSSH 9.3p2
    * OpenSSL 0.9.8zh/1.0.2u/1.1.1u/3.0.9
    * OpenVPN 2.4.12/2.5.9/2.6.5
    * pptp 1.10.0
--- make/pkgs/openssh/openssh.mk
+++ make/pkgs/openssh/openssh.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 9.3p1)
+$(call PKG_INIT_BIN, 9.3p2)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_HASH:=e9baba7701a76a51f3d85a62c383a3c9dcd97fa900b859bc7db114c1868af8a8
+$(PKG)_HASH:=200ebe147f6cb3f101fd0cdf9e02442af7ddca298dffd9f456878e7ccac676e8
 $(PKG)_SITE:=https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable,ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 ### WEBSITE:=https://www.openssh.com/
 ### MANPAGE:=https://www.openssh.com/manual.html
