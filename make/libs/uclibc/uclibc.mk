50b0b396fd01029ab18a23d0de1ab8d894057fd4
fda77 <fda77@users.noreply.github.com>
2023-08-07 14:35:07 +0200

    uclibc: add links

---
+++ make/libs/uclibc/uclibc.mk
@@ -0,0 +1,6 @@
+### WEBSITE:=https://uclibc-ng.org/
+### MANPAGE:=https://uclibc-ng.org/docs/
+### CHANGES:=https://gogs.waldemar-brodkorb.de/oss/uclibc-ng/releases
+### CVSREPO:=https://cgit.uclibc-ng.org/cgi/cgit/uclibc-ng.git/
+### VERSION:=0.9.28/0.9.29/0.9.32.1/0.9.33.2/1.0.14/1.0.44
+
