e2cb20bee1268df2b5e0d0a1eda9648d2c700148
fda77 <fda77@users.noreply.github.com>
2023-08-16 01:54:34 +0200

    tree: add links

--- make/pkgs/tree/tree.mk
+++ make/pkgs/tree/tree.mk
@@ -4,6 +4,10 @@ $(PKG)_HASH_ABANDON:=715d5d4b434321ce74706d0dd067505bb60c5ea83b5f0b3655dae40aa6f
 $(PKG)_HASH_CURRENT:=d3c3d55f403af7c76556546325aa1eca90b918cbaaf6d3ab60a49d8367ab90d5
 $(PKG)_HASH:=$($(PKG)_HASH_$(if $(FREETZ_PACKAGE_TREE_VERSION_ABANDON),ABANDON,CURRENT))
 $(PKG)_SITE:=https://mama.indstate.edu/users/ice/tree/src,ftp://mama.indstate.edu/linux/tree
+### WEBSITE:=http://mama.indstate.edu/users/ice/tree/
+### MANPAGE:=https://linux.die.net/man/1/tree
+### CHANGES:=https://gitlab.com/OldManProgrammer/unix-tree/tags
+### CVSREPO:=https://gitlab.com/OldManProgrammer/unix-tree
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/tree
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/tree
