106aa7d4b9524b3b99d6ca2f47d9e5076dddd016
fda77 <fda77@users.noreply.github.com>
2023-08-21 00:58:55 +0200

    wrapper-multid: check binary existence

--- patches/scripts/106-wrapper_multid.sh
+++ patches/scripts/106-wrapper_multid.sh
@@ -1,20 +1,26 @@
 
-echo1 "preparing multid wrapper"
+echo1 "preparing avm wrapper"
+
+file="${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
+# make sure PATH exists
+grep -q "^PATH=" "$file" || sed '2 i\PATH=$PATH' -i "$file"
+# extend PATH by wrapper
+modsed \
+  "s#\(^PATH=\)\(.*\)#\1/usr/bin/wrapper:\2#g" \
+  "$file" \
+  "^PATH=/usr/bin/wrapper:"
+
 
 wrapath="/usr/bin/wrapper/"
 for daemon in dsld multid rextd; do
+	[ -e "${FILESYSTEM_MOD_DIR}$wrapath$daemon" ] || continue
 	file="${FILESYSTEM_MOD_DIR}/lib/systemd/system/$daemon.service"
 	[ -e "$file" ] || continue
+
+	echo1 "preparing $daemon wrapper"
 	modsed -r \
 	  "s,^(ExecStart *=).*/*($daemon *.*)$,\1$wrapath\2," \
 	  "$file" \
 	  "$wrapath$daemon"
 done
-
-grep -q "^PATH=" "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net" || sed '2 i\PATH=$PATH' -i "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net"
-
-modsed \
-  "s#\(^PATH=\)\(.*\)#\1/usr/bin/wrapper:\2#g" \
-  "${FILESYSTEM_MOD_DIR}/etc/init.d/rc.net" \
-  "^PATH=/usr/bin/wrapper:"
 
