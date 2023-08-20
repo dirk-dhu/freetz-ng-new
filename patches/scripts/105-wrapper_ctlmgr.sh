7073bf833c3a743fe2c9b09e0e527f7665c70d27
fda77 <fda77@users.noreply.github.com>
2023-08-20 23:25:54 +0200

    libctlmgr: svctl compatibility

--- patches/scripts/105-wrapper_ctlmgr.sh
+++ patches/scripts/105-wrapper_ctlmgr.sh
@@ -1,5 +1,25 @@
-[ "$FREETZ_LIB_libctlmgr" == "y" ] || return 0
+[ "$FREETZ_AVM_HAS_AVMCTLMGR_PRELOAD" == "y" ] || return 0
 echo1 "preparing ctlmgr wrapper"
 
 create_LD_PRELOAD_wrapper /usr/bin/ctlmgr libctlmgr.so
+
+if [ -f "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ctlmgr.service" ] ; then
+	echo1 "preparing ctlmgr service"
+
+	ENV_FILE="/etc/.environment.ctlmgr"
+	# create env file
+	echo \
+	  'export LD_PRELOAD="libctlmgr.so${LD_PRELOAD:+:}${LD_PRELOAD}"' > \
+	  "${FILESYSTEM_MOD_DIR}/${ENV_FILE}"
+	# use own env file
+	modsed -r \
+	  "s/^(EnvironmentFile)=.*/\1=${ENV_FILE//\//\\/}/" \
+	  "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ctlmgr.service" \
+	  "=${ENV_FILE}$"
+	# dont use wrapper
+	modsed \
+	  's,^ExecStart=.*/,&avm/,' \
+	  "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ctlmgr.service" \
+	  "/avm/ctlmgr$"
+fi
 
