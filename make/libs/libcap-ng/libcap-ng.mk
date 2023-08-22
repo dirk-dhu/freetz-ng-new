24ea4f7f193a14f465701c8b08ae107f8487306e
fda77 <fda77@users.noreply.github.com>
2023-08-22 23:27:15 +0200

    libcap-ng: disable pthread.h  - refs https://github.com/Freetz-NG/freetz-ng/discussions/823

--- make/libs/libcap-ng/libcap-ng.mk
+++ make/libs/libcap-ng/libcap-ng.mk
@@ -16,6 +16,8 @@ $(PKG)_LA_STAGING_DIR   := $($(PKG)_LIBNAMES_SHORT:%=$(TARGET_TOOLCHAIN_STAGING_
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(AUTORECONF)
 
+$(PKG)_CONFIGURE_ENV += ac_cv_header_pthread_h=no
+
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
 $(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --prefix=/
