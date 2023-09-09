e97caa919554849f814db3a494af8ac7c263fcfd
fda77 <fda77@users.noreply.github.com>
2023-09-09 22:59:20 +0200

    bootmanager: supported_brandings needs correct pwd  - refs https://github.com/PeterPawn/YourFritz/issues/43

--- patches/scripts/800-yf_bootmanager.sh
+++ patches/scripts/800-yf_bootmanager.sh
@@ -3,18 +3,19 @@ echo1 "adding yf-bootmanager"
 
 [ "$FREETZ_VERBOSITY_LEVEL" -lt "2" ] 2>/dev/null && exec >/dev/null
 TEMPDIR=$(mktemp -d)
-pushd "${TOOLS_DIR}/yf/bootmanager" >/dev/null
 for oem in $(supported_brandings) all; do
 	www_oem="${FILESYSTEM_MOD_DIR}/usr/www/${oem}"
 	[ -d "${www_oem}" -a ! -L "${www_oem}" ] || continue
 
+	echo2 "processing branding: ${oem}"
+	pushd "${TOOLS_DIR}/yf/bootmanager" >/dev/null
 	TMP="$TEMPDIR" \
 	  TARGET_SYSTEM_VERSION="${AVM_FW_MAJOR}.${AVM_FW_VERSION}" \
-	  TARGET_DIR="${ABS_BASE_DIR}/${FILESYSTEM_MOD_DIR}" \
+	  TARGET_DIR="../../../${FILESYSTEM_MOD_DIR}" \
 	  TARGET_BRANDING="${oem}" \
-	  sh "./add_to_system_reboot.sh" 2>&1 | sed 's/^/    /g'
+	  sh "add_to_system_reboot.sh" 2>&1 | sed 's/^/      /g'
+	popd >/dev/null
 done
-popd >/dev/null
 rmdir "$TEMPDIR"
 [ "$FREETZ_VERBOSITY_LEVEL" -lt "2" ] 2>/dev/null && exec >$(tty)
 
