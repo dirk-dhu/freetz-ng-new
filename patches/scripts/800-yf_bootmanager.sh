fc7a236a1abb61d87ffb7b0e10b85e168b75671f
fda77 <fda77@users.noreply.github.com>
2023-09-09 18:59:00 +0200

    bootmanager: consider no-freetz mode branding, by @er13  - refs https://github.com/PeterPawn/YourFritz/issues/43

--- patches/scripts/800-yf_bootmanager.sh
+++ patches/scripts/800-yf_bootmanager.sh
@@ -4,11 +4,17 @@ echo1 "adding yf-bootmanager"
 [ "$FREETZ_VERBOSITY_LEVEL" -lt "2" ] 2>/dev/null && exec >/dev/null
 TEMPDIR=$(mktemp -d)
 pushd "${TOOLS_DIR}/yf/bootmanager" >/dev/null
-TMP="$TEMPDIR" \
-  TARGET_SYSTEM_VERSION="${AVM_FW_MAJOR}.${AVM_FW_VERSION}" \
-  TARGET_DIR="${ABS_BASE_DIR}/${FILESYSTEM_MOD_DIR}" \
-  TARGET_BRANDING="all" \
-  sh "./add_to_system_reboot.sh" 2>&1 | sed 's/^/    /g'
+for oem in $(supported_brandings) all; do
+	www_oem="${FILESYSTEM_MOD_DIR}/usr/www/${oem}"
+	[ -d "${www_oem}" -a ! -L "${www_oem}" ] || continue
+
+	TMP="$TEMPDIR" \
+	  TARGET_SYSTEM_VERSION="${AVM_FW_MAJOR}.${AVM_FW_VERSION}" \
+	  TARGET_DIR="${ABS_BASE_DIR}/${FILESYSTEM_MOD_DIR}" \
+	  TARGET_BRANDING="${oem}" \
+	  sh "./add_to_system_reboot.sh" 2>&1 | sed 's/^/    /g'
+done
 popd >/dev/null
 rmdir "$TEMPDIR"
 [ "$FREETZ_VERBOSITY_LEVEL" -lt "2" ] 2>/dev/null && exec >$(tty)
+
