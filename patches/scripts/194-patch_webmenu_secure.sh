c5f0d757e4fae84482c8ee993ab9b4916e4b558f
fda77 <fda77@users.noreply.github.com>
2023-11-08 12:40:34 +0100

    patches: fix indentation

--- patches/scripts/194-patch_webmenu_secure.sh
+++ patches/scripts/194-patch_webmenu_secure.sh
@@ -2,14 +2,14 @@
 echo1 "applying webmenu secure patch"
 
 for oem in $(supported_brandings) all; do
-   [ -d "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" -a ! -L "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" ] || continue
+	[ -d "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" -a ! -L "${FILESYSTEM_MOD_DIR}/usr/www/${oem}" ] || continue
 
-   for file in /usr/www/${oem}/home/home.js; do
-       [ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
-           [ "$FREETZ_PATCH_SECURE_2fa" == "y" ] && modsed 's/data[.]fritzos[.]twofactor_disabled/false/g'   ${FILESYSTEM_MOD_DIR}${file}
-           [ "$FREETZ_PATCH_SECURE_sip" == "y" ] && modsed 's/data[.]fritzos[.]ipphone_from_outside/false/g' ${FILESYSTEM_MOD_DIR}${file}
-           [ "$FREETZ_PATCH_SECURE_nau" == "y" ] && modsed 's/data[.]fritzos[.]noAutoupdate/false/g'         ${FILESYSTEM_MOD_DIR}${file}
-           [ "$FREETZ_PATCH_SECURE_nus" == "y" ] && modsed 's/data[.]fritzos[.]noUpdateSearch/false/g'       ${FILESYSTEM_MOD_DIR}${file}
-   done
+	for file in /usr/www/${oem}/home/home.js; do
+		[ ! -e ${FILESYSTEM_MOD_DIR}${file} ] && continue
+		[ "$FREETZ_PATCH_SECURE_2fa" == "y" ] && modsed 's/data[.]fritzos[.]twofactor_disabled/false/g'   ${FILESYSTEM_MOD_DIR}${file}
+		[ "$FREETZ_PATCH_SECURE_sip" == "y" ] && modsed 's/data[.]fritzos[.]ipphone_from_outside/false/g' ${FILESYSTEM_MOD_DIR}${file}
+		[ "$FREETZ_PATCH_SECURE_nau" == "y" ] && modsed 's/data[.]fritzos[.]noAutoupdate/false/g'         ${FILESYSTEM_MOD_DIR}${file}
+		[ "$FREETZ_PATCH_SECURE_nus" == "y" ] && modsed 's/data[.]fritzos[.]noUpdateSearch/false/g'       ${FILESYSTEM_MOD_DIR}${file}
+	done
 done
 
