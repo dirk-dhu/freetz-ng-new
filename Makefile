Coolzero82/freetz

NATIVE COMPILER: add target compiler and target tools as m4, automake, autoconf, bison and flex as a package, can be used as external and fix build problems also for Microsoft kernel

Makefile
@@ -237,8 +237,13 @@ TOOLCHAIN_DISTCLEAN:=$(patsubst %,%-distclean,$(TOOLCHAIN))
 TOOLCHAIN_SOURCE:=$(patsubst %,%-source,$(TOOLCHAIN))
 
 ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN)),y)
+ifneq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
 include $(TOOLCHAIN_DIR)/make/kernel-toolchain.mk
+endif
 include $(TOOLCHAIN_DIR)/make/target-toolchain.mk
+ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
+include $(TOOLCHAIN_DIR)/make/download-toolchain.mk
+endif
 else
 include $(TOOLCHAIN_DIR)/make/download-toolchain.mk
 endif

make/dev-tools-automake/dev-tools-automake.mk
@@ -1,0 +1,53 @@
+$(call PKG_INIT_BIN, 1.16.1)
+$(PKG)_SOURCE:=automake-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=7a4d8d8b16d2f3cece5ab92c32aba094a4597d97
+$(PKG)_SITE:=http://ftp.gnu.org/gnu/automake
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/automake-$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/bin/automake
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(DEV_TOOLS_PREFIX)/bin/automake
+
+$(PKG)_EXTRA_CFLAGS  := -ffunction-sections -fdata-sections
+$(PKG)_EXTRA_LDFLAGS := -Wl,--gc-sections
+
+$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's,^(C|LD)FLAGS[ \t]*=[ \t]*@\1FLAGS@,& $$$$(EXTRA_\1FLAGS),' \{\} \+;
+
+$(PKG)_CONFIGURE_OPTIONS := --prefix=$(DEV_TOOLS_PREFIX)
+$(PKG)_CONFIGURE_OPTIONS += --bindir=$(DEV_TOOLS_PREFIX)/bin
+$(PKG)_CONFIGURE_OPTIONS += --datadir=$(DEV_TOOLS_PREFIX)/share
+$(PKG)_CONFIGURE_OPTIONS += --includedir=$(DEV_TOOLS_PREFIX)/include
+$(PKG)_CONFIGURE_OPTIONS += --infodir=$(DEV_TOOLS_PREFIX)/share/info
+$(PKG)_CONFIGURE_OPTIONS += --libdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --libexecdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --mandir=$(DEV_TOOLS_PREFIX)/share/man
+$(PKG)_CONFIGURE_OPTIONS += --sbindir=$(DEV_TOOLS_PREFIX)/sbin
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(DEV_TOOLS_AUTOMAKE_DIR) \
+		EXTRA_CFLAGS="$(DEV_TOOLS_AUTOMAKE_EXTRA_CFLAGS)" \
+		EXTRA_LDFLAGS="$(DEV_TOOLS_AUTOMAKE_EXTRA_LDFLAGS)" \
+		STRIP="$(TARGET_STRIP)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(DEV_TOOLS_AUTOMAKE_DIR) \
+		DESTDIR="$(abspath $(DEV_TOOLS_AUTOMAKE_DEST_DIR))" \
+		install-strip; \
+	$(RM) -rf $(dir $@)../share/info $(dir $@)../share/man $(dir $@)../share/doc
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(DEV_TOOLS_AUTOMAKE_DIR) clean
+	$(RM) $(DEV_TOOLS_AUTOMAKE_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(DEV_TOOLS_AUTOMAKE_TARGET_BINARY)
+
+
+$(PKG_FINISH)

make/dev-tools-m4/dev-tools-m4.mk
@@ -1,0 +1,54 @@
+$(call PKG_INIT_BIN, 1.4.18)
+$(PKG)_SOURCE:=m4-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=a077779db287adf4e12a035029002d28
+$(PKG)_SITE:=http://ftp.gnu.org/gnu/m4/
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/m4-$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/m4
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(DEV_TOOLS_PREFIX)/bin/m4
+
+$(PKG)_EXTRA_CFLAGS  := -ffunction-sections -fdata-sections
+$(PKG)_EXTRA_LDFLAGS := -Wl,--gc-sections
+
+$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's,^(C|LD)FLAGS[ \t]*=[ \t]*@\1FLAGS@,& $$$$(EXTRA_\1FLAGS),' \{\} \+;
+$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's~LDADD = ../lib/libm4.a(.*)~LDADD = ../lib/libm4.a\1 \$$$\$$$\(LIB_POSIX_SPAWN\)~'   \{\} \+;
+
+$(PKG)_CONFIGURE_OPTIONS := --prefix=$(DEV_TOOLS_PREFIX)
+$(PKG)_CONFIGURE_OPTIONS += --bindir=$(DEV_TOOLS_PREFIX)/bin
+$(PKG)_CONFIGURE_OPTIONS += --datadir=$(DEV_TOOLS_PREFIX)/share
+$(PKG)_CONFIGURE_OPTIONS += --includedir=$(DEV_TOOLS_PREFIX)/include
+$(PKG)_CONFIGURE_OPTIONS += --infodir=$(DEV_TOOLS_PREFIX)/share/info
+$(PKG)_CONFIGURE_OPTIONS += --libdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --libexecdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --mandir=$(DEV_TOOLS_PREFIX)/share/man
+$(PKG)_CONFIGURE_OPTIONS += --sbindir=$(DEV_TOOLS_PREFIX)/sbin
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(DEV_TOOLS_M4_DIR) \
+		EXTRA_CFLAGS="$(DEV_TOOLS_M4_EXTRA_CFLAGS)" \
+		EXTRA_LDFLAGS="$(DEV_TOOLS_M4_EXTRA_LDFLAGS)" \
+		STRIP="$(TARGET_STRIP)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(DEV_TOOLS_M4_DIR) \
+		DESTDIR="$(abspath $(DEV_TOOLS_M4_DEST_DIR))" \
+		install-strip; \
+	$(RM) -rf $(dir $@)../share
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(DEV_TOOLS_M4_DIR) clean
+	$(RM) $(DEV_TOOLS_M4_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(DEV_TOOLS_M4_TARGET_BINARY)
+
+
+$(PKG_FINISH)

dirk-dhu/freetz-ng-new

Makefile
@@ -398,8 +398,13 @@ TOOLCHAIN_DISTCLEAN:=$(patsubst %,%-distclean,$(TOOLCHAIN))
 TOOLCHAIN_SOURCE:=$(patsubst %,%-source,$(TOOLCHAIN))
 
 ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN)),y)
+ifneq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
 include $(MAKE_DIR)/toolchain/kernel-toolchain.mk
+endif
 include $(MAKE_DIR)/toolchain/target-toolchain.mk
+ifeq ($(strip $(FREETZ_BUILD_TOOLCHAIN_TARGET_ONLY)),y)
+include $(MAKE_DIR)/toolchain/download-toolchain.mk
+endif
 else
 include $(MAKE_DIR)/toolchain/download-toolchain.mk
 endif

make/pkgs/dev-tools-automake/dev-tools-automake.mk
@@ -1,0 +1,53 @@
+$(call PKG_INIT_BIN, 1.16.1)
+$(PKG)_SOURCE:=automake-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=7a4d8d8b16d2f3cece5ab92c32aba094a4597d97
+$(PKG)_SITE:=http://ftp.gnu.org/gnu/automake
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/automake-$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/bin/automake
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(DEV_TOOLS_PREFIX)/bin/automake
+
+$(PKG)_EXTRA_CFLAGS  := -ffunction-sections -fdata-sections
+$(PKG)_EXTRA_LDFLAGS := -Wl,--gc-sections
+
+$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's,^(C|LD)FLAGS[ \t]*=[ \t]*@\1FLAGS@,& $$$$(EXTRA_\1FLAGS),' \{\} \+;
+
+$(PKG)_CONFIGURE_OPTIONS := --prefix=$(DEV_TOOLS_PREFIX)
+$(PKG)_CONFIGURE_OPTIONS += --bindir=$(DEV_TOOLS_PREFIX)/bin
+$(PKG)_CONFIGURE_OPTIONS += --datadir=$(DEV_TOOLS_PREFIX)/share
+$(PKG)_CONFIGURE_OPTIONS += --includedir=$(DEV_TOOLS_PREFIX)/include
+$(PKG)_CONFIGURE_OPTIONS += --infodir=$(DEV_TOOLS_PREFIX)/share/info
+$(PKG)_CONFIGURE_OPTIONS += --libdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --libexecdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --mandir=$(DEV_TOOLS_PREFIX)/share/man
+$(PKG)_CONFIGURE_OPTIONS += --sbindir=$(DEV_TOOLS_PREFIX)/sbin
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(DEV_TOOLS_AUTOMAKE_DIR) \
+		EXTRA_CFLAGS="$(DEV_TOOLS_AUTOMAKE_EXTRA_CFLAGS)" \
+		EXTRA_LDFLAGS="$(DEV_TOOLS_AUTOMAKE_EXTRA_LDFLAGS)" \
+		STRIP="$(TARGET_STRIP)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(DEV_TOOLS_AUTOMAKE_DIR) \
+		DESTDIR="$(abspath $(DEV_TOOLS_AUTOMAKE_DEST_DIR))" \
+		install-strip; \
+	$(RM) -rf $(dir $@)../share/info $(dir $@)../share/man $(dir $@)../share/doc
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(DEV_TOOLS_AUTOMAKE_DIR) clean
+	$(RM) $(DEV_TOOLS_AUTOMAKE_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(DEV_TOOLS_AUTOMAKE_TARGET_BINARY)
+
+
+$(PKG_FINISH)

make/pkgs/dev-tools-m4/dev-tools-m4.mk
@@ -1,0 +1,54 @@
+$(call PKG_INIT_BIN, 1.4.18)
+$(PKG)_SOURCE:=m4-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=a077779db287adf4e12a035029002d28
+$(PKG)_SITE:=http://ftp.gnu.org/gnu/m4/
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/m4-$($(PKG)_VERSION)
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/m4
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(DEV_TOOLS_PREFIX)/bin/m4
+
+$(PKG)_EXTRA_CFLAGS  := -ffunction-sections -fdata-sections
+$(PKG)_EXTRA_LDFLAGS := -Wl,--gc-sections
+
+$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's,^(C|LD)FLAGS[ \t]*=[ \t]*@\1FLAGS@,& $$$$(EXTRA_\1FLAGS),' \{\} \+;
+$(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -type f -exec $(SED) -i -r -e 's~LDADD = ../lib/libm4.a(.*)~LDADD = ../lib/libm4.a\1 \$$$\$$$\(LIB_POSIX_SPAWN\)~'   \{\} \+;
+
+$(PKG)_CONFIGURE_OPTIONS := --prefix=$(DEV_TOOLS_PREFIX)
+$(PKG)_CONFIGURE_OPTIONS += --bindir=$(DEV_TOOLS_PREFIX)/bin
+$(PKG)_CONFIGURE_OPTIONS += --datadir=$(DEV_TOOLS_PREFIX)/share
+$(PKG)_CONFIGURE_OPTIONS += --includedir=$(DEV_TOOLS_PREFIX)/include
+$(PKG)_CONFIGURE_OPTIONS += --infodir=$(DEV_TOOLS_PREFIX)/share/info
+$(PKG)_CONFIGURE_OPTIONS += --libdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --libexecdir=$(DEV_TOOLS_PREFIX)/lib
+$(PKG)_CONFIGURE_OPTIONS += --mandir=$(DEV_TOOLS_PREFIX)/share/man
+$(PKG)_CONFIGURE_OPTIONS += --sbindir=$(DEV_TOOLS_PREFIX)/sbin
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(DEV_TOOLS_M4_DIR) \
+		EXTRA_CFLAGS="$(DEV_TOOLS_M4_EXTRA_CFLAGS)" \
+		EXTRA_LDFLAGS="$(DEV_TOOLS_M4_EXTRA_LDFLAGS)" \
+		STRIP="$(TARGET_STRIP)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(DEV_TOOLS_M4_DIR) \
+		DESTDIR="$(abspath $(DEV_TOOLS_M4_DEST_DIR))" \
+		install-strip; \
+	$(RM) -rf $(dir $@)../share
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(DEV_TOOLS_M4_DIR) clean
+	$(RM) $(DEV_TOOLS_M4_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(DEV_TOOLS_M4_TARGET_BINARY)
+
+
+$(PKG_FINISH)