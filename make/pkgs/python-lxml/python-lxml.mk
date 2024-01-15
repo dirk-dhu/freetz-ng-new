dirk-dhu/freetz-old

python: add python modules pyusb, pyyaml, lxml, configobj, six, pygooglechart, certbot acme, zope, mock, ndg-httsclient, pyasn, pycparser, idna, augeas and much more

make/python-lxml/python-lxml.mk
@@ -1,0 +1,37 @@
+$(call PKG_INIT_BIN, 3.4.0)
+$(PKG)_SOURCE:=lxml-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=bc90cc4e4ee04e1f8290ae0f70e34eea
+$(PKG)_SITE:=https://pypi.python.org/packages/source/l/lxml
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/lxml-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/lxml/etree.so
+
+$(PKG)_DEPENDS_ON += python xsltproc libxml2
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+$(PKG)_REBUILD_SUBOPTS += $(XSLTPROC_REBUILD_SUBOPTS)
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libxml2
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libxslt
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libexslt
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_LXML, , TARGET_ARCH=$(FREETZ_TARGET_ARCH))
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_LXML_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_LXML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/lxml \
+		$(PYTHON_LXML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/lxml-*.egg-info
+
+$(PKG_FINISH)

make/python-pyyaml/python-pyyaml.mk
@@ -1,0 +1,34 @@
+$(call PKG_INIT_BIN, 3.11)
+$(PKG)_SOURCE:=PyYAML-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=f50e08ef0fe55178479d3a618efe21db
+$(PKG)_SITE:=https://pypi.python.org/packages/source/P/PyYAML
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/PyYAML-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/_yaml.so
+
+$(PKG)_DEPENDS_ON += python
+$(PKG)_DEPENDS_ON += yaml
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libyaml
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_PYYAML)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_PYYAML_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PYYAML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/yaml \
+		$(PYTHON_PYYAML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/PyYAML-*.egg-info
+
+$(PKG_FINISH)

make/python-websocket-client/python-websocket-client.mk
@@ -1,0 +1,34 @@
+$(call PKG_INIT_BIN, 0.47.0)
+$(PKG)_SOURCE:=v$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=2c70e7eccf407a5d5cc5c101dc87e078
+$(PKG)_SITE:=https://github.com/websocket-client/websocket-client/archive
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/websocket-client-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/websocket
+
+$(PKG)_DEPENDS_ON += python
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_WEBSOCKET_CLIENT) && \
+	touch $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_WEBSOCKET_CLIENT_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_WEBSOCKET_CLIENT_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/websocket \
+		$(PYTHON_WEBSOCKET_CLIENT_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/websocket_client-*.egg-info
+
+$(PKG_FINISH)

dirk-dhu/freetz-ng-new

make/pkgs/python-lxml/python-lxml.mk
@@ -1,0 +1,37 @@
+$(call PKG_INIT_BIN, 3.4.0)
+$(PKG)_SOURCE:=lxml-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=bc90cc4e4ee04e1f8290ae0f70e34eea
+$(PKG)_SITE:=https://pypi.python.org/packages/source/l/lxml
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/lxml-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/lxml/etree.so
+
+$(PKG)_DEPENDS_ON += python xsltproc libxml2
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+$(PKG)_REBUILD_SUBOPTS += $(XSLTPROC_REBUILD_SUBOPTS)
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libxml2
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libxslt
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libexslt
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_LXML, , TARGET_ARCH=$(FREETZ_TARGET_ARCH))
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_LXML_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_LXML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/lxml \
+		$(PYTHON_LXML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/lxml-*.egg-info
+
+$(PKG_FINISH)

make/pkgs/python-pyyaml/python-pyyaml.mk
@@ -1,0 +1,34 @@
+$(call PKG_INIT_BIN, 3.11)
+$(PKG)_SOURCE:=PyYAML-$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=f50e08ef0fe55178479d3a618efe21db
+$(PKG)_SITE:=https://pypi.python.org/packages/source/P/PyYAML
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/PyYAML-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/_yaml.so
+
+$(PKG)_DEPENDS_ON += python
+$(PKG)_DEPENDS_ON += yaml
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libyaml
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_PYYAML)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_PYYAML_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_PYYAML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/yaml \
+		$(PYTHON_PYYAML_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/PyYAML-*.egg-info
+
+$(PKG_FINISH)

make/pkgs/python-websocket-client/python-websocket-client.mk
@@ -1,0 +1,34 @@
+$(call PKG_INIT_BIN, 0.47.0)
+$(PKG)_SOURCE:=v$($(PKG)_VERSION).tar.gz
+$(PKG)_HASH:=2c70e7eccf407a5d5cc5c101dc87e078
+$(PKG)_SITE:=https://github.com/websocket-client/websocket-client/archive
+
+$(PKG)_DIR:=$($(PKG)_SOURCE_DIR)/websocket-client-$($(PKG)_VERSION)
+
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/websocket
+
+$(PKG)_DEPENDS_ON += python
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+	$(call Build/PyMod/PKG, PYTHON_WEBSOCKET_CLIENT) && \
+	touch $@
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r $(PYTHON_WEBSOCKET_CLIENT_DIR)/build
+
+$(pkg)-uninstall:
+	$(RM) -r \
+		$(PYTHON_WEBSOCKET_CLIENT_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/websocket \
+		$(PYTHON_WEBSOCKET_CLIENT_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/websocket_client-*.egg-info
+
+$(PKG_FINISH)